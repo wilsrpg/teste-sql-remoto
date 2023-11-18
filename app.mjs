4f0a16900ea5f903ff5924fa62acc4c497a6e8f3
wilsrpg <wilson.rpg@hotmail.com>
2023-11-18 00:16:01 -0300

    implementei conexão com mongodb

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import mysql from 'mysql2'
 //import { config as dotenvConfig } from 'dotenv';
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
 //dotenvConfig();
 
@@ -17,17 +18,22 @@ servidor.get('/', async (req, resp)=>{
   resp.sendFile('index.html', {root: '.'});
 });
 
-servidor.post('/', async (req, resp) => {
+servidor.post('/mysql', async (req, resp) => {
   const con = mysql.createConnection({
-    host: req.body.servidor,
+    host: req.body.servidor.slice(0,req.body.servidor.search(':')),
+    port: req.body.servidor.slice(req.body.servidor.search(':')),
     database: req.body.banco,
     user: req.body.usuario,
     password: req.body.senha
   });
+  //console.log(con.config.host+':'+con.config.port);
+  //console.log(con.config.database);
+  //console.log(con.config.user);
 
   const desconectado = await new Promise(resolve=>{
     con.ping(erro=>resolve(erro));
   });
+  console.log(desconectado);
   if(desconectado)
     return resp.sendFile('erro.html', {root: '.'});
   
@@ -40,6 +46,33 @@ servidor.post('/', async (req, resp) => {
   //  });
   //});
   //console.log(usuario);
+
+  resp.sendFile('sucesso.html', {root: '.'});
+});
+
+servidor.post('/mongodb', async (req, resp) => {
+  const uri = 'mongodb+srv://'+req.body.usuario+':'+req.body.senha+'@'+req.body.servidor+'/?retryWrites=true&w=majority';
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(erro) {
+    console.log('Erro na conexão com o banco de dados: '+erro);
+    return resp.sendFile('erro.html', {root: '.'});
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 
   resp.sendFile('sucesso.html', {root: '.'});
 });
